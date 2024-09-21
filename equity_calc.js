8657f9cf46be9d8efda73874cdbef43474264e1b
quentincornier <quentincornier95@gmail.com>
2024-09-21 12:29:50 +0200

    Ajout d'une conversion dans la notation de la main d'un joueur pour faciliter les calculs

--- equity_calc.js
+++ equity_calc.js
@@ -1,17 +1,20 @@
 import { createShodown } from './shodown.js';
-import {evalHands} from './hand_evaluation.js';
+import { evalHands } from './hand_evaluation.js';
+import { convertHand } from './hand_conversion.js';
 
 export function equity(players, board) {
 
     let SIMULATION = 10000;
 
+    let playersHand = convertHand(players)
+
     let win = new Array(players.length).fill(0);
     let winRate = new Array(players.length);
 
     for (let simulation = 0; simulation < SIMULATION; simulation++) {
-        let shodown = createShodown(board);
+        let shodown = createShodown(playersHand, board)
 
-        let bestHandIndex = evalHands(players, shodown);
+        let bestHandIndex = evalHands(playersHand, shodown);
 
         win[bestHandIndex]++;
     }
---
+++ hand_conversion.js
@@ -0,0 +1,82 @@
+export function convertHand(players) {
+    /*
+    On récupère des mains sous la forme :
+        [ ["Ac", "6h"], ["Ts", "7s"], ... ]
+
+    On veut utliser des valeurs chiffrées donc on va convertir chaque carte en une liste de deux éléments
+        "Ac" -> [14, 3]
+
+    Liste des équivalences :
+        1 -> 1
+        2 -> 3
+        ...
+        9 -> 9
+        T -> 10
+        J -> 11
+        Q -> 12
+        K -> 13
+        A -> 14
+
+        et 
+
+        h -> 1
+        s -> 2
+        c -> 3
+        d -> 4
+    */
+
+    let convertedHands = new Array(players.length);
+
+    for (let player = 0; player < players.length; player++) {
+        let hand = new Array(2);
+
+        for (let card = 0; card < 2; card++) {
+            let suit = 0;
+            let value = 0;
+
+            let stringValue = players[player][card][0];
+            let stringSuit = players[player][card][1];
+
+            switch (stringValue) {
+                case "A":
+                    value = 14;
+                    break;
+                case "K":
+                    value = 13;
+                    break;
+                case "Q":
+                    value = 12;
+                    break;
+                case "J":
+                    value = 11;
+                    break;
+                case "T":
+                    value = 10;
+                    break;
+                default:
+                    value = parseInt(stringValue);
+            }
+
+            switch (stringSuit) {
+                case "h":
+                    suit = 1;
+                    break;
+                case "s":
+                    suit = 2;
+                    break;
+                case "c":
+                    suit = 3;
+                    break;
+                case "d":
+                    suit = 4;
+                    break;
+            }
+
+            hand[card] = [value, suit];
+        }
+
+        convertHand[player] = hand;
+    }
+
+    return convertHand;
+}
